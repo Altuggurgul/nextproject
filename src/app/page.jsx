4e93dd97af920d4a9178c98616dbe3ca7d6ca2a7
Altug Gurgul <84250064+Altuggurgul@users.noreply.github.com>
2024-10-25 18:52:03 +0300

    raporlar guncellendi

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -258,7 +258,7 @@ export default function Example() {
         </div>
         {/* TAB BÖLÜMÜ */}
         <Listbox value={selectedCategory} onChange={setSelectedCategory}>
-      <label className="block text-sm font-medium leading-6 text-gray-900">Kategori Seç</label>
+      <label className="hidden text-sm font-medium leading-6 text-gray-900">Kategori Seç</label>
       <div className="relative mt-2 w-full max-w-xs">
         <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
           <span className="block truncate">{selectedCategory}</span>
